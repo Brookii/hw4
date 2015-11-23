46b9e62b6dd17b0d9e9b1de60b0726575f8b1ad1
Student <martin.boeckmann@uni-duesseldorf.de>
2015-11-23 19:03:17 +0100

    Viel Spaß mit dem Filter!

---
+++ filter.cxx
@@ -0,0 +1,54 @@
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void reading(const int N, double* p, const string noisy);
+void Ausgabe_auf_englisch(const int N, double* p, const string noisy);
+void filtern(const int N, double* p, const string noisy);
+
+int main(){
+	const int N = 237;
+	const string noisy = "noisy.txt";
+	double* p = new double[N];
+	
+	reading(N,p,noisy);
+	filtern(N,p,noisy);
+	Ausgabe_auf_englisch(N,p,noisy);
+	
+	delete[] p;
+	return 0;
+}
+
+void filtern(const int N, double* p, const string noisy){
+	double altp, neup = p[N-1];
+		for(int u = 0; u < 3;u++){
+			for(int i = 0; i < N; i++){
+				altp = p[i];
+				p[i] = (neup + p[i] +p[i+1])/3.0;
+				neup = altp;
+			}
+		}
+}
+
+void reading(const int N, double* p, const string noisy){
+	ifstream in(noisy.c_str());
+		for(int i = 0; i < N; i++){
+			in >> p[i];
+		}
+	in.close();
+}
+
+void Ausgabe_auf_englisch(const int N, double* p, const string noisy){
+	ofstream out("gefiltert_3.txt");
+	for(int i = 0; i < N; i++){
+		out << p[i] << endl;
+	}	
+	out.close();
+}
+
+
+
